7e70b6c30ec576ca51f01723717b88f9e4c45b0a
Khemchand Patel <khemchandpatel784@gmail.com>
2024-04-25 18:07:04 +0530

    ui changes

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,60 +1,36 @@
-"use client";
-import { useState } from "react";
-import { Menu, Transition } from "@headlessui/react";
-import { IoChevronDown } from "react-icons/io5";
+import React from "react";
 
 interface DropdownMenuProps {
-  options: string[];
+  title: string;
+  content: string;
 }
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ options }) => {
-  const [menu, setMenu] = useState(options[0]); // Initialize menu with the first option
-
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ title, content }) => {
   return (
-    <div className="relative">
-      <Menu as="div" className="relative text-left">
-        <div>
-          <Menu.Button className="flex items-center justify-between py-4 w-full rounded-md bg-white px-4 text-sm border-gray-200 border-solid border-[1px] focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-            {menu}
-            <IoChevronDown size={20} />
-          </Menu.Button>
-        </div>
-        <Transition
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none z-50 w-full">
-            {options.map((option, index) => (
-              <Menu.Item key={index}>
-                {/* {({ active }) => (
-                  <button
-                    onClick={() => {
-                      setMenu(option);
-                    }}
-                    className={`${
-                      active ? "bg-violet-500 text-white" : "text-gray-900"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {option}
-                  </button>
-                )} */}
-                <button
-                  onClick={() => {
-                    setMenu(option);
-                  }}
-                  className={`text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                >
-                  {option}
-                </button>
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </Transition>
-      </Menu>
+    <div className="">
+      <details className="group border-2 border-neutral-200 px-6 py-2 rounded-md">
+        <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
+          <span className="text-[16px] font-light">{title}</span>
+          <span className="transition group-open:rotate-180">
+            <svg
+              fill="none"
+              height="24"
+              shapeRendering="geometricPrecision"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              width="24"
+            >
+              <path d="M6 9l6 6 6-6"></path>
+            </svg>
+          </span>
+        </summary>
+        <p className="text-neutral-600 mt-3 group-open:animate-fadeIn text-left text-sm">
+          {content}
+        </p>
+      </details>
     </div>
   );
 };
