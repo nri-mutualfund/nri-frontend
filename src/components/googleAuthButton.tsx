46028143f6a73c5ad62f765c4c50cc0770a5be2e
PSchristopher <pschristopher7@gmail.com>
2024-05-02 18:41:25 +0530

    signIn signUp page

---
+++ src/components/googleAuthButton.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const GoogleAuthButton = () => {
+    const router = useRouter();
+    const handleSignIn = async() => {
+        // Replace 'YOUR_GOOGLE_CLIENT_ID' and 'YOUR_REDIRECT_URI' with actual values
+        const googleAuthUrl = await `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=989700434149-agf796mtutj8nudb4euqq0s2ounj47o0.apps.googleusercontent.com&redirect_uri=http://localhost:5000/v1.0/auth/oauth/google&scope=email%20profile&state=http://localhost:3000/success`;
+    console.log(googleAuthUrl, 'googleAuthUrlgoogleAuthUrl')
+        // Redirect user to Google's authorization endpoint
+        // router.push(googleAuthUrl);
+        router.push(googleAuthUrl);
+
+    };
+    
+  return (
+    <div className='flex w-3/4 justify-center gap-8 rounded-md bg-white border-solid border-gray-300 border-[1px] px-3 py-1.5 '>
+      <img src="/assets/Google.png" width={30} alt="" />
+        <button  onClick={()=>handleSignIn()} className="text-lg  leading-6 text-black shadow-sm ">  Continue with Google</button>
+    </div>
+  )
+}
+
+export default GoogleAuthButton
