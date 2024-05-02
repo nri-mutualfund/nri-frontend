2770c517289f3f1bbad3492fe0d1184439fdec9c
Khemchand Patel <khemchandpatel784@gmail.com>
2024-05-02 18:35:02 +0530

    change slider design

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -10,7 +10,8 @@ import "swiper/css/navigation";
 
 // import required modules
 import Image from "next/image";
-import { FaPlay } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa6";
+import { FaChevronRight } from "react-icons/fa6";
 const ImageSlider = () => {
   const swiperRef = useRef<Swiper | null>(null);
   const swiperRef2 = useRef<Swiper | null>(null);
@@ -40,17 +41,11 @@ const ImageSlider = () => {
 
   return (
     <div className="flex gap-4 md:gap-8 items-center justify-center">
-      <button
-        className="h-[50px] w-[50px] flex md:hidden  justify-center items-center rounded-lg bg-gradient-to-r from-[#0080004D] to-white"
-        onClick={handlePrevClick2}
-      >
-        <FaPlay color="#fff" size={30} className="rotate-180" />
+      <button className="flex md:hidden" onClick={handlePrevClick2}>
+        <FaChevronLeft color="#6C6C6C" size={30} />
       </button>
-      <button
-        className="h-[70px] w-[70px] hidden md:flex justify-center items-center rounded-lg bg-gradient-to-r from-[#0080004D] to-white"
-        onClick={handlePrevClick}
-      >
-        <FaPlay color="#fff" size={30} className="rotate-180" />
+      <button className="hidden md:flex " onClick={handlePrevClick}>
+        <FaChevronLeft color="#6C6C6C" size={40} />
       </button>
       <div className="w-[60%] lg:w-1/2  bg-white block md:hidden">
         <Swiper
@@ -61,64 +56,7 @@ const ImageSlider = () => {
         >
           {[1, 2, 3, 4, 5, 6, 7, 8, 9]?.map((item, index) => (
             <SwiperSlide className=" bg-white " key={index}>
-              <div className="bg-white rounded-lg py-8 px-6 border-gradient drop-shadow-lg">
-                <Image
-                  src={"/assets/cords.png"}
-                  alt="cots"
-                  className="w-[20px]"
-                  width={500}
-                  height={1}
-                />
-                <p className="font-light text-sm mt-4 leading-[16px]">
-                  Lorem ipsum dolor sit amet consectetur. Massa commodo gravida
-                  urna vel mattis. Lorem ipsum dolor sit amet consectetur. Massa
-                  commodo gravida urna vel mattis. Lorem ipsum dolor sit amet
-                  consectetur.
-                </p>
-                <div className="flex gap-4 items-center mt-10">
-                  <Image
-                    src={
-                      index % 2 === 0
-                        ? "/assets/female.png"
-                        : "/assets/male.png"
-                    }
-                    alt="cots"
-                    className="w-[60px]"
-                    width={500}
-                    height={1}
-                  />
-                  <div>
-                    <p className="font-medium text-gray-800 leading-[18px]">
-                      {index % 2 === 0 ? "Navya Roy" : "Siddharth Soni"}
-                    </p>
-                    <p className="font-medium text-gray-800 text-sm leading-[16px] mt-2">
-                      {index % 2 === 0 ? "Music Enthusiast" : "Audiophile"}
-                    </p>
-                    <Image
-                      src="/assets/star.png"
-                      alt="cots"
-                      className="w-[50px] mt-1"
-                      width={500}
-                      height={1}
-                    />
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className="w-[70%] lg:w-1/2  bg-white hidden md:block">
-        <Swiper
-          slidesPerView={2}
-          spaceBetween={20}
-          loop={true}
-          ref={swiperRef}
-          className="mySwiper bg-white"
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9]?.map((item, index) => (
-            <SwiperSlide className=" bg-white " key={index}>
-              <div className="bg-white rounded-lg py-8 px-6 border-gradient drop-shadow-lg">
+              <div className="bg-white rounded-xl border-2 py-12 px-6 drop-shadow-xl">
                 <Image
                   src={"/assets/cords.png"}
                   alt="cots"
@@ -165,17 +103,68 @@ const ImageSlider = () => {
           ))}
         </Swiper>
       </div>
-      <button
-        className="h-[50px]  w-[50px] flex md:hidden justify-center items-center rounded-lg bg-gradient-to-l from-[#0080004D] to-white"
-        onClick={handleNextClick2}
-      >
-        <FaPlay color="#fff" size={30} />
+      <div className="w-[70%] lg:w-[55%]  bg-white hidden md:block">
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={20}
+          loop={true}
+          ref={swiperRef}
+          className="mySwiper"
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9]?.map((item, index) => (
+            <SwiperSlide className=" bg-white " key={index}>
+              <div className="bg-white rounded-xl border-2 py-12 px-6 drop-shadow-xl">
+                <Image
+                  src={"/assets/cords.png"}
+                  alt="cots"
+                  className="w-[20px]"
+                  width={500}
+                  height={1}
+                />
+                <p className="font-light text-sm mt-4 leading-[16px]">
+                  Lorem ipsum dolor sit amet consectetur. Massa commodo gravida
+                  urna vel mattis. Lorem ipsum dolor sit amet consectetur. Massa
+                  commodo gravida urna vel mattis. Lorem ipsum dolor sit amet
+                  consectetur.
+                </p>
+                <div className="flex gap-4 items-center mt-10">
+                  <Image
+                    src={
+                      index % 2 === 0
+                        ? "/assets/female.png"
+                        : "/assets/male.png"
+                    }
+                    alt="cots"
+                    className="w-[60px]"
+                    width={500}
+                    height={1}
+                  />
+                  <div>
+                    <p className="font-medium text-gray-800">
+                      {index % 2 === 0 ? "Navya Roy" : "Siddharth Soni"}
+                    </p>
+                    <p className="font-medium text-gray-800 text-sm">
+                      {index % 2 === 0 ? "Music Enthusiast" : "Audiophile"}
+                    </p>
+                    <Image
+                      src="/assets/star.png"
+                      alt="cots"
+                      className="w-[50px] mt-1"
+                      width={500}
+                      height={1}
+                    />
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <button className="flex md:hidden " onClick={handleNextClick2}>
+        <FaChevronRight color="#6C6C6C" size={30} />
       </button>
-      <button
-        className="h-[70px] w-[70px] hidden md:flex justify-center items-center rounded-lg bg-gradient-to-l from-[#0080004D] to-white"
-        onClick={handleNextClick}
-      >
-        <FaPlay color="#fff" size={30} />
+      <button className="hidden md:flex " onClick={handleNextClick}>
+        <FaChevronRight color="#6C6C6C" size={40} />
       </button>
     </div>
   );
