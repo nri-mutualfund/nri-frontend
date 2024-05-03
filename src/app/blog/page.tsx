740025e3c2f9998948245a3c517c69ca081a4f9f
Khemchand Patel <khemchandpatel784@gmail.com>
2024-05-03 14:01:25 +0530

    redesign blog section

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,20 +2,11 @@ import ImageSlider from "@/components/ImageSlider";
 import React from "react";
 
 const Page = () => {
-  const images = [
-    "/assets/long-term-wealth.png",
-    "/assets/long-term-wealth.png",
-    "/assets/long-term-wealth.png",
-    "/assets/long-term-wealth.png",
-    "/assets/long-term-wealth.png",
-    "/assets/long-term-wealth.png",
-  ];
-
   return (
     <div>
       <h1 className="text-center">Blogs</h1>
       <div className="container mx-auto py-8">
-        <ImageSlider images={images} />
+        <ImageSlider />
       </div>
     </div>
   );
