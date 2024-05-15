c84f4d7916e5dbbbe9e1b8d5e48a5c9956f7eb64
Khemchand Patel <khemchandpatel784@gmail.com>
2024-05-15 14:44:08 +0530

    text alignment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,12 +77,12 @@ const Page = () => {
             <svg
               aria-hidden="true"
               viewBox="0 0 418 42"
-              className="h-10 w-[250px] md:w-[400px] fill-[#00800040] absolute top-20 md:top-12 right-[90px] md:right-0 -z-20"
+              className="h-10 w-[250px] md:w-[300px] fill-[#00800040] absolute top-20 md:top-10 right-[90px] md:right-30 -z-20"
               preserveAspectRatio="none"
             >
               <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
             </svg>
-            <h2 className="text-gray-800 mx-auto text-5xl md:text-7xl  leading-[40px] md:leading-[80px]">
+            <h2 className="text-gray-800 mx-auto text-5xl md:text-6xl  leading-[55px] md:leading-[70px]">
               The easiest and <span className="text-primary">fastest way </span>
               to Invest in India
             </h2>
@@ -211,8 +211,8 @@ const Page = () => {
           </div>
         </div>
       </section> */}
-      <section className="px-4 md:px-20 lg:px-40 mt-10">
-        <div className="bg-white py-24 sm:py-32">
+      <section className="px-4 md:px-20 lg:px-40 mt-10 bg-secondary">
+        <div className="py-24 sm:py-32">
           <div className="mx-auto max-w-7xl">
             <div className="mx-auto max-w-2xl text-center">
               {/* <h2 className="text-base font-semibold leading-7 text-primary">
@@ -221,7 +221,7 @@ const Page = () => {
               <h2 className="mt-2 font-bold tracking-tight text-gray-900">
                 A one-stop <span className="text-primary">Indian</span> Expats
               </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
+              <p className="mt-1 text-sm text-gray-600">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu,
                 mollis aenean sit dictum tincidunt. Ut arcu, suscipit ac etLorem
                 ipsum dolor sit amet.Lorem ipsum dolor sit amet, consectetur
@@ -266,9 +266,16 @@ const Page = () => {
       </section> */}
       {/* section 4 */}
       <section className=" px-4 md:px-20 lg:px-40 mt-24">
-        <h2 className="text-gray-800 mx-auto  font-semibold font-graphik leading-[40px]">
-          Product <span className="text-primary">We offer</span>
-        </h2>
+        <div className="w-full lg:w-1/2">
+          <h2 className="text-gray-800 mx-auto  font-semibold font-graphik leading-[40px]">
+            Product <span className="text-primary">We offer</span>
+          </h2>
+          <p className="text-sm font-light mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu,
+            mollis aenean sit dictum tincidunt. Ut arcu,
+          </p>
+        </div>
+
         <div className="flex gap-12 justify-between mt-8">
           <div className="w-full flex flex-col gap-4 lg:hidden">
             {expandIndex === 0 ? (
@@ -819,16 +826,23 @@ const Page = () => {
         </div>
       </section>
       {/* section 5 */}
-      <section className=" px-4 md:px-20 lg:px-40 mt-32 py-32">
+      <section className=" px-4 md:px-20 lg:px-40 mt-32 py-32 bg-secondary">
         <div className="grid grid-cols-1 gap-x-20 gap-y-10 mt-4 lg:grid-cols-2 items-center">
           <div className="w-full">
             <h2 className="text-gray-800  font-semibold font-graphik leading-[40px] max-w-80">
               Open <span className="text-primary">NRI Bank Account </span>in
               India
             </h2>
-            <p className="font-light mt-4 leading-7">
+            <p className="font-light mt-1 text-sm">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
               egestas tempus tellus etiam sed. Quam a scelerisque amet
+              ullamcorper eu enim et fermentum, augue. 
+              Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam
+              sed. Quam a scelerisque amet ullamcorper eu enim et fermentum,
+              augue.
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Et, egestas tempus tellus etiam sed. Quam a scelerisque amet
               ullamcorper eu enim et fermentum, augue.
             </p>
             <div className="flex flex-wrap gap-2">
@@ -922,11 +936,15 @@ const Page = () => {
         </div>
       </section>
       {/* section 6 */}
-      <section className=" px-4 md:px-20 lg:px-40 mt-10 lg:mt-32">
-        <div className="w-full flex justify-center items-center">
-          <h2 className="text-gray-800 mx-auto  font-semibold font-graphik">
+      <section className="px-4 md:px-20 lg:px-40 mt-10 lg:mt-32">
+        <div className="w-full flex flex-col justify-center items-center">
+          <h2 className="text-gray-800 mx-auto  font-semibold font-graphik leading-[40px]">
             <span className="text-primary">NRI</span> Services
           </h2>
+          <p className="font-light text-sm mt-1 text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu,
+            mollis aenean sit dictum tincidunt. Ut arcu,
+          </p>
         </div>
 
         <div className="max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-14 mx-auto mt-10">
@@ -1083,12 +1101,12 @@ const Page = () => {
         </div>
       </section>
       {/* {section 7} */}
-      <section className=" px-4 md:px-20 lg:px10 mt-24">
+      <section className="px-4 md:px-20 lg:px-10 mt-24 py-12">
         <div className="w-full flex flex-col justify-center items-center">
           <h2 className="text-gray-800 mx-auto  font-semibold font-graphik leading-[40px] text-center">
             What <span className="text-primary">Customers</span> have to say
           </h2>
-          <p className="font-light text-sm mt-4 text-center">
+          <p className="font-light text-sm mt-1 text-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu,
             mollis aenean sit dictum tincidunt. Ut arcu,
           </p>
@@ -1103,7 +1121,7 @@ const Page = () => {
         </div>
       </section>
       {/* {section 8} */}
-      <section className=" px-4 md:px-20 lg:px-40 mt-24">
+      <section className=" px-4 md:px-20 lg:px-40 mt-24 bg-secondary py-32">
         <div className="w-full flex flex-col justify-center items-center">
           <h2 className="text-gray-800 mx-auto font-semibold font-graphik leading-[40px] text-center">
             Your money will be{" "}
