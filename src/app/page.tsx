212fd75ca8531b085bc765dcd9f2fceb1051e795
Khemchand Patel <khemchandpatel784@gmail.com>
2024-05-17 16:07:53 +0530

    Ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1233,7 +1233,7 @@ const Page = () => {
           </div>
         </div>
       </section> */}
-      <section
+      {/* <section
         className="px-4 md:px-20 lg:px-40 mt-12 h-36 bg-cover bg-center bg-no-repeat block md:hidden py-4"
         style={{ backgroundImage: "url('/assets/community2.png')" }}
       >
@@ -1248,20 +1248,40 @@ const Page = () => {
             </button>
           </div>
         </div>
+      </section> */}
+      <section className="px-4 md:px-20 lg:px-40 mt-32  md:hidden block">
+        <div
+          className="bg-cover bg-center bg-no-repeat rounded-[30px] py-6 drop-shadow-md"
+          style={{ backgroundImage: "url('/assets/community2.png')" }}
+        >
+          <div className="flex justify-center items-center">
+            <div>
+              <p className="text-lg  text-center max-w-[250px] text-[#111827]">
+                Join our Whatsapp community of NRI/OCIs like you
+              </p>
+              <button className="flex gap-2 mx-auto px-6 items-center mt-6 py-2 rounded-md bg-primary  text-white focus:outline-none ease transition-all hover:shadow-lg hover:-translate-y-1 duration-300">
+                <FaWhatsapp size={20} color="#fff" />
+                Join Community
+              </button>
+            </div>
+          </div>
+        </div>
       </section>
-      <section
-        className="px-4 md:px-20 lg:px-40 mt-32  hidden md:block bg-cover bg-center bg-no-repeat h-20 md:h-56 xl:h-[350px]"
-        style={{ backgroundImage: "url('/assets/community2.png')" }}
-      >
-        <div className="flex justify-center items-center h-full">
-          <div>
-            <p className="font-semibold text-xl text-center max-w-[320px] text-[#111827]">
-              Join our Whatsapp community of NRI/OCIs like you
-            </p>
-            <button className="flex gap-2 mx-auto px-6 items-center mt-6 py-2 rounded-md bg-primary  text-white focus:outline-none ease transition-all hover:shadow-lg hover:-translate-y-1 duration-300">
-              <FaWhatsapp size={20} color="#fff" />
-              Join Community
-            </button>
+      <section className="px-4 md:px-20 lg:px-40 mt-32  hidden md:block ">
+        <div
+          className="bg-cover bg-center bg-no-repeat rounded-[30px] py-8 drop-shadow-md lg:py-16"
+          style={{ backgroundImage: "url('/assets/community2.png')" }}
+        >
+          <div className="flex justify-center items-center h-full">
+            <div>
+              <p className="font-semibold text-xl text-center max-w-[320px] text-[#111827]">
+                Join our Whatsapp community of NRI/OCIs like you
+              </p>
+              <button className="flex gap-2 mx-auto px-6 items-center mt-6 py-2 rounded-md bg-primary  text-white focus:outline-none ease transition-all hover:shadow-lg hover:-translate-y-1 duration-300">
+                <FaWhatsapp size={20} color="#fff" />
+                Join Community
+              </button>
+            </div>
           </div>
         </div>
       </section>
