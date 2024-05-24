9f566aba48073e9a7f634c009f9b309e9dff75ae
Khemchand Patel <khemchandpatel784@gmail.com>
2024-05-24 18:41:51 +0530

    integrated apis to fetch data

---
+++ src/components/SectionFour.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { GoArrowRight } from "react-icons/go";
+type DataProps = {
+  data: object;
+};
+const SectionFour = ({ data }: DataProps) => {
+  const [showArrow, setShowArrow] = useState(false);
+  return (
+    <section className=" px-4 md:px-20 lg:px-40  py-14 md:py-24 lg:py-32 bg-secondary">
+      <div className="grid grid-cols-1 gap-x-20 gap-y-10 mt-4 lg:grid-cols-2 items-center">
+        <div className="w-full">
+          <h2 className="text-gray-800  font-medium  leading-[40px] max-w-80">
+            {data?.pre_heading}{" "}
+            <span className="text-primary">{data?.highlightned}</span>{" "}
+            {data?.post_heading}
+          </h2>
+          <p className="text-sm font-light mt-1">{data?.detail}</p>
+          <div className="flex flex-wrap gap-2">
+            <div className="flex  items-start mt-10">
+              <button className="px-8  py-2  text-white  bg-primary rounded-md shadow active:bg-primary focus:outline-none ease transition-all hover:shadow-lg hover:-translate-y-1 duration-300">
+                Create Account
+              </button>
+            </div>
+            <button
+              className="flex gap-4  px-6 items-center mt-10 py-2 rounded-md bg-white border border-primary text-primary focus:outline-none ease transition-all hover:shadow-lg hover:-translate-y-1 duration-300"
+              onMouseEnter={() => {
+                setShowArrow(true);
+              }}
+              onMouseLeave={() => {
+                setShowArrow(false);
+              }}
+            >
+              Contact us
+              {showArrow && <GoArrowRight size={20} color="#008000" />}
+            </button>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="grid max-w-xl grid-cols-2 gap-x-8 gap-y-6 items-center">
+            <div className="w-full">
+              <Image
+                src="/assets/AXIS_LOGO2.png"
+                alt="Your Image Alt Text"
+                className="w-[180px]"
+                width={500}
+                height={1}
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/assets/IDFC_LOGO2.png"
+                alt="Your Image Alt Text"
+                className="w-[180px]"
+                width={500}
+                height={1}
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/assets/ICICI_LOGO2.png"
+                alt="Your Image Alt Text"
+                className="w-[180px] pb-[30px]"
+                width={500}
+                height={1}
+              />
+            </div>
+            <div className="w-full">
+              <Image
+                src="/assets/HDFC_LOGO2.png"
+                alt="Your Image Alt Text"
+                className="w-[180px]"
+                width={500}
+                height={1}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionFour;
