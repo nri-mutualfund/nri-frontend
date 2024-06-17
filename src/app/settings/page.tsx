a7f92f931d1ed570c293e1759a510fbcbff3d32e
Khemchand Patel <khemchandpatel784@gmail.com>
2024-06-17 18:42:49 +0530

    fix responsive ness for mobile devices

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,74 +1,90 @@
-'use client'
-import React, { useState } from 'react'
-import SettingProfile from '../../components/settingProfile';
-import Documents from '@/components/Documents';
-import BankAccount from '@/components/BankAccount';
-import Nominiees from '@/components/Nominees';
-import { CgProfile } from 'react-icons/cg';
-import { BsBank } from 'react-icons/bs';
-import { IoDocumentTextOutline, IoPeopleCircleOutline } from 'react-icons/io5';
-type Tab = 'profile' | 'documents' | 'bankAccounts' | 'nominees';
+"use client";
+import React, { useState } from "react";
+import SettingProfile from "../../components/settingProfile";
+import Documents from "@/components/Documents";
+import BankAccount from "@/components/BankAccount";
+import Nominiees from "@/components/Nominees";
+import { CgProfile } from "react-icons/cg";
+import { BsBank } from "react-icons/bs";
+import { IoDocumentTextOutline, IoPeopleCircleOutline } from "react-icons/io5";
+type Tab = "profile" | "documents" | "bankAccounts" | "nominees";
 
 const Settings = () => {
-    const [activeTab, setActiveTab] = useState<Tab>('profile');
+  const [activeTab, setActiveTab] = useState<Tab>("profile");
 
-    const renderContent = () => {
-        switch (activeTab) {
-            case 'profile':
-                return <SettingProfile />;
-            case 'documents':
-                return <Documents/>;
-            case 'bankAccounts':
-                return <BankAccount />;
-            case 'nominees':
-                return <Nominiees />;
-            default:
-                return <SettingProfile />;
-        }
-    };
+  const renderContent = () => {
+    switch (activeTab) {
+      case "profile":
+        return <SettingProfile />;
+      case "documents":
+        return <Documents />;
+      case "bankAccounts":
+        return <BankAccount />;
+      case "nominees":
+        return <Nominiees />;
+      default:
+        return <SettingProfile />;
+    }
+  };
   return (
-
-    <div className='bg-gray-100 py-10'>
-    <div className="flex justify-center  shadow-lg mx-56  rounded-xl">
-            <section className="flex justify-start flex-col  border-r border-gray-400  bg-white w-1/4 py-8  rounded-l-xl">
-                <button
-                    className={`flex items-center gap-4 justify-start mb-2  px-6 py-2  ${activeTab === 'profile' ? ' text-primary bg-green-50 border-l-4 border-green-700' : 'text-[#666666]'}`}
-                    onClick={() => setActiveTab('profile')}
-                >
+    <div className="max-w-screen-2xl mx-auto">
+      <div className="px-10 md:px-20 lg:px-40  py-14">
+        <div className="md:border md:rounded-lg md:p-14 mt-10 md:bg-white md:shadow-sm">
+          <div className="grid grid-cols-1 md:grid-cols-4 justify-center">
+            <section className="flex justify-start flex-col  md:border-r border-gray-400  bg-white col-span-1 py-8  rounded-l-xl">
+              <button
+                className={`flex items-center gap-4 justify-start mb-2  px-6 py-2  ${
+                  activeTab === "profile"
+                    ? " text-primary bg-green-50 border-l-4 border-green-700"
+                    : "text-[#666666]"
+                }`}
+                onClick={() => setActiveTab("profile")}
+              >
                 <CgProfile />
                 Profile
-                </button>
-                <button
-                    className={`flex  items-center gap-4 justify-start mb-2  px-6 py-2  ${activeTab === 'documents' ? ' text-primary bg-green-50 border-l-4 border-green-700' : 'text-[#666666]'}`}
-                    onClick={() => setActiveTab('documents')}
-                >
-<IoDocumentTextOutline />
-
-                    Documents
-                </button>
-                <button
-                    className={`flex  items-center gap-4 justify-start mb-2  px-6 py-2  ${activeTab === 'bankAccounts' ? ' text-primary bg-green-50 border-l-4 border-green-700' : 'text-[#666666]'}`}
-                    onClick={() => setActiveTab('bankAccounts')}
-                >
-                    <BsBank />
-
-                    Bank Account
-                </button>
-                <button
-                    className={`flex  items-center gap-4 justify-start mb-2  px-6 py-2  ${activeTab === 'nominees' ? ' text-primary bg-green-50 border-l-4 border-green-700' : 'text-[#666666]'}`}
-                    onClick={() => setActiveTab('nominees')}
-                >
-             <IoPeopleCircleOutline />
-             Nominees
-                </button>
-                
+              </button>
+              <button
+                className={`flex  items-center gap-4 justify-start mb-2  px-6 py-2  ${
+                  activeTab === "documents"
+                    ? " text-primary bg-green-50 border-l-4 border-green-700"
+                    : "text-[#666666]"
+                }`}
+                onClick={() => setActiveTab("documents")}
+              >
+                <IoDocumentTextOutline />
+                Documents
+              </button>
+              <button
+                className={`flex  items-center gap-4 justify-start mb-2  px-6 py-2  ${
+                  activeTab === "bankAccounts"
+                    ? " text-primary bg-green-50 border-l-4 border-green-700"
+                    : "text-[#666666]"
+                }`}
+                onClick={() => setActiveTab("bankAccounts")}
+              >
+                <BsBank />
+                Bank Account
+              </button>
+              <button
+                className={`flex  items-center gap-4 justify-start mb-2  px-6 py-2  ${
+                  activeTab === "nominees"
+                    ? " text-primary bg-green-50 border-l-4 border-green-700"
+                    : "text-[#666666]"
+                }`}
+                onClick={() => setActiveTab("nominees")}
+              >
+                <IoPeopleCircleOutline />
+                Nominees
+              </button>
             </section>
-            <section className="content p-4  bg-white rounded w-3/4 rounded-r-xl">
-                {renderContent()}
+            <section className="content md:p-4  bg-white rounded rounded-r-xl grid-cols-1 md:col-span-3">
+              {renderContent()}
             </section>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
--- src/components/BankAccount.tsx
+++ src/components/BankAccount.tsx
@@ -1,31 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const BankAccount = () => {
   return (
-    <div className="p-6 lg:col-span-9">
-        <div className="sm:flex sm:items-center">
-            <div className="sm:flex-auto">
-                <h3 className="text-lg font-medium leading-6 text-gray-700">Bank Accounts</h3>
-            </div>
-            </div>
-            <div className="py-24 text-center">
-                <div className="justify-center text-gray-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" className="mx-auto h-16 w-16">
-                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"></path>
-                    </svg>
-
-                </div>
-                <h3 className="mt-2 text-md font-semibold text-gray-700">Get started by finishing your application</h3>
-                 <div className="mt-6">
-                    <button type="submit" className="inline-flex items-center rounded-md bg-primary px-4 py-3 text-md font-semibold text-white shadow-sm hover:bg-primary500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
-                    Finish onboarding       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" className="ml-2 h-5 w-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"></path>
-                    </svg></button>
-                 </div>
-         
-            </div>
+    <div className="md:p-6 lg:col-span-9">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h3 className="text-lg font-medium leading-6 text-gray-700">
+            Bank Accounts
+          </h3>
+        </div>
+      </div>
+      <div className="py-24 text-center">
+        <div className="justify-center text-gray-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden="true"
+            data-slot="icon"
+            className="mx-auto h-16 w-16"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
+            ></path>
+          </svg>
+        </div>
+        <h3 className="mt-2 text-md font-semibold text-gray-700">
+          Get started by finishing your application
+        </h3>
+        <div className="mt-6">
+          <button
+            type="submit"
+            className="inline-flex items-center rounded-md bg-primary px-4 py-3 text-md font-semibold text-white shadow-sm hover:bg-primary500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+          >
+            Finish onboarding{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+              className="ml-2 h-5 w-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BankAccount
+export default BankAccount;
