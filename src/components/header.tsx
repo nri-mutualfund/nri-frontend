7f5baddfeac3720fc02314c85741ec256fac526d
Khemchand Patel <khemchandpatel784@gmail.com>
2024-06-19 17:28:19 +0530

    routing fix and added logout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
 import NProgress from "nprogress";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { FaRegUserCircle } from "react-icons/fa";
+import { Menu, Transition } from "@headlessui/react";
+import nProgress from "nprogress";
+
 const Header = () => {
   const pathname = usePathname();
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -139,18 +142,70 @@ const Header = () => {
           >
             Sign in
           </Link> */}
-          {token && pathname === "/settings" ? (
-            <Link
-              href={"/"}
-              onClick={() => {
-                NProgress.start();
-              }}
-            >
-              <FaRegUserCircle
-                size={25}
-                className="text-text_dark hover:text-primary"
-              />
-            </Link>
+          {token ? (
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button>
+                  <FaRegUserCircle size={25} className="hover:text-primary" />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href={"settings"}
+                          onClick={() => {
+                            if (pathname !== "/settings") {
+                              nProgress.start();
+                            }
+                          }}
+                        >
+                          <button
+                            className={`${
+                              active ? "text-primary" : "text-text_dark"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Settings
+                          </button>
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href={"/"}
+                          onClick={() => {
+                            localStorage.clear();
+                            setToken(null);
+                            if (pathname !== "/") {
+                              nProgress.start();
+                            }
+                          }}
+                        >
+                          <button
+                            className={`${
+                              active ? "text-primary" : "text-text_dark"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Logout
+                          </button>
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           ) : (
             <Link
               href={"/signup"}
@@ -167,22 +222,78 @@ const Header = () => {
           )}
         </div>
 
-        <div className="items-center flex gap-8 lg:hidden" onClick={handleNav}>
-          {token && pathname === "/settings" ? (
-            <Link
-              href={"/"}
-              onClick={() => {
-                NProgress.start();
-              }}
-            >
-              <FaRegUserCircle size={25} />
-            </Link>
+        <div className="items-center flex gap-8 lg:hidden">
+          {token ? (
+            <Menu as="div" className="relative text-left">
+              <div>
+                <Menu.Button className={""}>
+                  <FaRegUserCircle size={25} className="hover:text-primary" />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href={"settings"}
+                          onClick={() => {
+                            if (pathname !== "/settings") {
+                              nProgress.start();
+                            }
+                          }}
+                        >
+                          <button
+                            className={`${
+                              active ? "text-primary" : "text-text_dark"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Settings
+                          </button>
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href={"/"}
+                          onClick={() => {
+                            localStorage.clear();
+                            setToken(null);
+                            if (pathname !== "/") {
+                              nProgress.start();
+                            }
+                          }}
+                        >
+                          <button
+                            className={`${
+                              active ? "text-primary" : "text-text_dark"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Logout
+                          </button>
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           ) : (
             <Link
               href={"/signup"}
               onClick={() => {
                 if (pathname !== "/signup") {
                   NProgress.start();
+                  handleNav();
                 }
               }}
             >
