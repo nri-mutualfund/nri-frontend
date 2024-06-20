30a72e0ff2498b97794d7b636b0c1e23fea37210
PSchristopher <pschristopher7@gmail.com>
2024-06-20 12:31:35 +0530

    changed redirection

--- src/components/googleAuthButton.tsx
+++ src/components/googleAuthButton.tsx
@@ -7,7 +7,7 @@ const GoogleAuthButton = () => {
   const handleSignIn = async () => {
     // Replace 'YOUR_GOOGLE_CLIENT_ID' and 'YOUR_REDIRECT_URI' with actual values
     const googleClientId = '623164060170-vapn304ccckpdbr1lupnk8tjn16g2uo3.apps.googleusercontent.com';
-    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${googleClientId}&redirect_uri=http://localhost:5000/v1.0/auth/oauth/google&scope=email%20profile&state=http://localhost:3000/verify-pan`;
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${googleClientId}&redirect_uri=https://nriapi.shlokaconcepts.com/v1.0/auth/oauth/google&scope=email%20profile&state=https://nri-frontend.vercel.app/verify-pan`;
     console.log(googleAuthUrl, "googleAuthUrlgoogleAuthUrl");
     // Redirect user to Google's authorization endpoint
     // router.push(googleAuthUrl);
